5cb844e2cc89ad42557414569808aced239b58be
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2025-02-26 10:51:07 +0600

    feat: create theme in darkmode with change option

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
+import { ThemeProvider } from "./context/theme-provider";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>Hello</Layout>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>Hello</Layout>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,10 +7,10 @@ function Layout({ children }: PropsWithChildren) {
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
-      <footer className="border-t backdrop-blur py-12">
+      <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-4 text-center">
           <p>
-            All Right Resereved by
+            All Right Reserved by
             <span className="font-bold text-rose-600"> Sumonta056</span>
           </p>
         </div>
